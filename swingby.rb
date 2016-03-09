e1c99b8548d8a7680f88d3fe6d5aed25578c8074
j-hibiki <serviceace.toshi@gmail.com>
2016-03-10 06:20:24 +0900

    commit2

--- swingby.rb
+++ swingby.rb
@@ -8,21 +8,22 @@ JUP_Y = 50
 
 sat_vx = 2
 sat_vy = 3
+v_start = (sat_vx*sat_vx + sat_vy*sat_vy)**0.5
 
-for i in 0..100
+for i in 0..20
 
-  dis_x = JUP_X - sat_x
-  dis_y = JUP_Y - sat_y
-  dis = (dis_x**2 + dis_y**2)**(0.5)
+  dx = JUP_X - sat_x
+  dy = JUP_Y - sat_y
+  dist = (dx*dx + dy*dy)**0.5
 
-  puts "dis_x = #{dis_x}"
-  puts "dis_y = #{dis_y}"
-  puts "dis = #{dis}"
+  # puts "dist_x = #{dist_x}"
+  # puts "dist_y = #{dist_y}"
+  # puts "dist = #{dist}"
 
-  sat_a = 100 / dis / dis
+  sat_a = 1000 / dist / dist
 
-  sat_ax = sat_a * dis_x / dis
-  sat_ay = sat_a * dis_y / dis
+  sat_ax = sat_a * dx / dist
+  sat_ay = sat_a * dy / dist
 
   sat_vx += sat_ax
   sat_vy += sat_ay
@@ -33,4 +34,10 @@ for i in 0..100
   puts "sat_x=#{sat_x}"
   puts "sat_y=#{sat_y}"
   puts ""
+
 end
+
+v_end = (sat_vx*sat_vx + sat_vy*sat_vy)**0.5
+got_speed = v_end - v_start
+
+puts "you got #{got_speed} by swingby"
